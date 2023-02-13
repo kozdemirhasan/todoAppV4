403df93210c052d31aeab87e17acf3dec9a02c95
Hasan Murat Kücüközdemir <kozdemirhasan@gmail.com>
2023-02-13 08:20:28 +0100

    update

--- src/main/java/de/kozdemir/todoappv4/TodoAppV4Application.java
+++ src/main/java/de/kozdemir/todoappv4/TodoAppV4Application.java
@@ -10,7 +10,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import java.time.LocalDateTime;
@@ -35,18 +38,9 @@ public class TodoAppV4Application implements WebMvcConfigurer, CommandLineRunner
         SpringApplication.run(TodoAppV4Application.class, args);
     }
 
-    @Bean
-    public LocaleResolver localeResolver() {
-        Locale.setDefault(new Locale(uiLanguage));
-        //CookieLocaleResolver resolver = new CookieLocaleResolver();
-        SessionLocaleResolver resolver = new SessionLocaleResolver();
-        resolver.setDefaultLocale(new Locale(uiLanguage));
-        return resolver;
-    }
 
     @Override
     public void run(String... args) throws Exception {
-
 
         if(dbReset) {
             todoRepository.deleteAll();
@@ -63,8 +57,34 @@ public class TodoAppV4Application implements WebMvcConfigurer, CommandLineRunner
 
             admin = new User("p.parker@shield.com", "geheim123");
             userRepository.save(admin);
-
-
         }
     }
+
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        Locale.setDefault(new Locale(uiLanguage));
+       /*
+        CookieLocaleResolver resolver = new CookieLocaleResolver();
+        resolver.setCookieName("lang");
+        resolver.setCookieMaxAge(60*60*24*30);
+        */
+        SessionLocaleResolver resolver = new SessionLocaleResolver();
+        resolver.setDefaultLocale(new Locale(uiLanguage));
+        return resolver;
+    }
+
+    /* Erlaubt das Ändern der Sprache über die URL, http://www.domain.de?lang=de */
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
+        interceptor.setParamName("lang");
+        return interceptor;
+    }
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+
 }
